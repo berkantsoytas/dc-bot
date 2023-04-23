cddb835b8319d38ea3e0e886baffc6b8839875be
Mert Sefa AKGUN <git@msakg.com>
2023-04-23 17:03:35 +0300

    feat(bot): add sample code

--- inc/bot.h
+++ inc/bot.h
@@ -1,4 +1,7 @@
 #ifndef BOT_H
 #define BOT_H
-
+void print_usage(void);
+void on_ready(struct discord *client, const struct discord_ready *event);
+void on_ping(struct discord *client, const struct discord_message *event);
+void on_pong(struct discord *client, const struct discord_message *event);
 #endif // !BOT_H
